da98df911a410dc9371b5664afed824e933b4b1c
Yash Raka <167842425+yash-raka@users.noreply.github.com>
2026-08-09 00:48:17 +0530

    Time: 0 ms (100%), Space: 8.9 MB (79.53%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string largestGoodInteger(string num) {
         string x = "";
     
-        for (int i=0; i<num.size()-2; i++){
+        for (int i=0;i<num.size()-2;i++){
             if (num[i] == num[i+1] && num[i+1] == num[i+2]){
                 string arr = "";
                 
