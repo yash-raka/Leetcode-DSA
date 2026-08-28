4dbbba2b96de5475ac04b8237c9efb2e2f563fa6
Yash Raka <167842425+yash-raka@users.noreply.github.com>
2026-08-28 16:48:35 +0530

    Time: 7 ms (55.64%), Space: 128.3 MB (77.9%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,21 +5,16 @@ public:
     vector<int> ans(n);
 
     int x = 0;
+    int y = 1;
     for (int i=0;i<nums.size();i++){
         if (nums[i] > 0){
             ans[x] = nums[i];
             x = x+2;
-        }
-    }
-
-    int y = 1;
-    for (int i=0;i<nums.size();i++){
-        if (nums[i] < 0){
+        } else {
             ans[y] = nums[i];
             y = y+2;
         }
     }
-    
     return ans;
     }
 };
