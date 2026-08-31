8b09847ab98f255ecab5315a399d6f3bedcd7d7f
Yash Raka <167842425+yash-raka@users.noreply.github.com>
2026-08-31 09:58:42 +0530

    Time: 0 ms (100%), Space: 30.2 MB (79.57%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     vector<int> concatWithReverse(vector<int>& nums) {
-        vector<int> ans;
-
-        for (int i=0;i<nums.size();i++){
-            ans.push_back(nums[i]);
-        }
+        vector<int> ans = nums;
         for (int i=nums.size()-1;i>=0;i--){
             ans.push_back(nums[i]);
         }
