aa9aa33c0ac74e2b00091dc9a6a944c4ae64b84f
Yash Raka <167842425+yash-raka@users.noreply.github.com>
2026-09-04 15:39:04 +0530

    Time: 223 ms (27.77%), Space: 117.9 MB (12.47%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int countDistinctIntegers(vector<int>& nums) {
         vector<int> ans = nums;
-        int x = 0;
         for (int i=0;i<nums.size();i++){
+            int x = 0;
             while (nums[i] != 0){
                 x = x*10 + nums[i]%10;
-                nums[i] = nums[i] / 10;
+                nums[i] = nums[i]/10;
             }
             ans.push_back(x);
-            x = 0;
         }
-
-        for (int i=0;i<ans.size();i++){
-            cout << ans[i] << " ";
-        }
-
         unordered_map<int,  int> arr;
         for (int i=0;i<ans.size();i++){
             arr[ans[i]]++;
